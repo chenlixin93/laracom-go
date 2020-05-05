1651aba8dd7351f1a83abf5a2a79eef05738f967
jasper <jasper.chen@sain3.com>
2020-05-05 17:12:09 +0800

    re deomo-service

--- demo-service/go.mod
+++ demo-service/go.mod
@@ -3,11 +3,16 @@ module laracom/demo-service
 go 1.14
 
 require (
+	github.com/go-log/log v0.2.0 // indirect
 	github.com/golang/protobuf v1.4.1
-	github.com/gorilla/mux v1.7.4
-	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
+	github.com/gorilla/mux v1.7.3
+	github.com/micro/go-micro v1.18.0
+	github.com/miekg/dns v1.1.29 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	golang.org/x/net v0.0.0-20200505041828-1ed23360d12c
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/grpc v1.29.1
-	google.golang.org/protobuf v1.22.0
+	google.golang.org/grpc v1.25.1
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- demo-service/main.go
+++ demo-service/main.go
@@ -2,69 +2,27 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	"laracom/demo-service/api"
+	"github.com/micro/go-micro"
 	pb "laracom/demo-service/proto/demo"
 	"log"
-	"net"
 )
 
-const (
-	address = "localhost:9091"
-	grpcPort = ":9999"
-	httpPort = "8000"
-	appName = "Demo Service"
-)
-
-type DemoService struct {
+type DemoServiceHandler struct {
 
 }
 
-func (ds *DemoService) SayHello(ctx context.Context, req *pb.DemoRequest) (*pb.DemoResponse, error) {
-	rsp := &pb.DemoResponse{Text: "你好, " + req.Name}
-	return rsp, nil
+func (s *DemoServiceHandler) SayHello(ctx context.Context, req *pb.DemoRequest, rsp *pb.DemoResponse) error {
+	rsp.Text = "你好, " + req.Name
+	return nil
 }
 
-func main() {
-	// 通过-mode参数判断启动哪个模式的代码
-	mode := flag.String("mode", "grpc", "mode:grpc/http/client")
-	flag.Parse()
-	fmt.Println("run mode:", *mode)
-	switch *mode {
-	case "http":
-		fmt.Printf("Starting %v\n", appName)
-		api.StartWebServer(httpPort)
-	case "client":
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("连接到 gRPC 服务器失败: %v", err)
-		}
-		defer conn.Close()
-		client := pb.NewDemoServiceClient(conn)
-		req := &pb.DemoRequest{Name: "学院君"}
-		rsp, err := client.SayHello(context.Background(), req)
-		if err != nil {
-			log.Fatalf("调用 gRPC 服务接口失败: %v", err)
-		}
-		log.Printf("%s", rsp.Text)
-	case "grpc":
-		fallthrough
-	default:
-		listener, err := net.Listen("tcp", grpcPort)
-		if err != nil {
-			log.Fatalf("监听指定端口失败: %v", err)
-		}
+func main()  {
+	// 注册服务名必须和 demo.proto 中的 package 声明一致
+	service := micro.NewService(micro.Name("laracom.demo.service"))
+	service.Init()
 
-		server := grpc.NewServer()
-		pb.RegisterDemoServiceServer(server, &DemoService{})
-
-		reflection.Register(server)
-
-		if err := server.Serve(listener); err != nil {
-			log.Fatalf("服务启动失败: %v", err)
-		}
+	pb.RegisterDemoServiceHandler(service.Server(), &DemoServiceHandler{})
+	if err := service.Run(); err != nil {
+		log.Fatalf("服务启动失败: %v", err)
 	}
 }
