e967eeea5ed77327ab92f4150e34fab37bead3c9
jasper <jasper.chen@sain3.com>
2020-05-06 23:33:21 +0800

    用户注册接口

---
+++ user-service/go.mod
@@ -0,0 +1,12 @@
+module laracom-go/user-service
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/jinzhu/gorm v1.9.12
+	github.com/micro/go-micro v1.18.0
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
+	golang.org/x/net v0.0.0-20191109021931-daa7c04131f5
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+)
---
+++ user-service/repo/repository.go
@@ -0,0 +1,50 @@
+package repo
+
+import (
+	"github.com/jinzhu/gorm"
+	pb "laracom-go/user-service/proto/user"
+)
+
+type Repository interface {
+	Create(user *pb.User) error
+	Get(id string) (*pb.User, error)
+	GetByEmail(email string) (*pb.User, error)
+	GetAll() ([]*pb.User, error)
+}
+
+type UserRepository struct {
+	Db *gorm.DB
+}
+
+func (repo *UserRepository) Create(user *pb.User) error {
+	if err := repo.Db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
+	var user *pb.User
+	user.Id = id
+	if err := repo.Db.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) GetByEmail(email string) (*pb.User, error) {
+	user := &pb.User{}
+	if err := repo.Db.Where("email = ?", email).
+		First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
+	if err := repo.Db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
